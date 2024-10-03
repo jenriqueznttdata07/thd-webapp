eefd4d6810ee284e52875af2d254d1552d4e9458
Jesus Enriquez <jesus.enriquez@nttdata.com>
2024-10-03 13:33:40 -0600

    feat: main layout added

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import "@/styles/Footer.css";
+
+interface RootLayoutProps {
+    children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+        <html lang="en">
+            <head>
+                <title>The Home Depot</title>
+                <meta name="description" content="The Home Depot Webapp for bootcamp" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
